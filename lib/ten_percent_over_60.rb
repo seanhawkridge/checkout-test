85c5c8cb5f5b9d38f48f4d59223bf07371b867bd
seanhawkridge <sean@hawkridge.co>
2016-04-30 08:15:12 +0100

    Refactoring ten percent over 60 offer
    - pushing calculation into private method

--- lib/ten_percent_over_60.rb
+++ lib/ten_percent_over_60.rb
@@ -2,6 +2,12 @@
 class TenPercentOver60
 
   def reduction(items, total)
+    calculate_reduction(items, total)
+  end
+
+  private
+
+  def calculate_reduction(items, total)
     total > 60 ? total *= 0.1 : 0
   end
 
