57e7961c5df7513f5822fc562c56da564432d85b
seanhawkridge <sean@hawkridge.co>
2016-04-30 08:40:23 +0100

    Refactoring lavender hearts offer

--- lib/lavender_hearts_multiple_offer.rb
+++ lib/lavender_hearts_multiple_offer.rb
@@ -8,12 +8,16 @@ class LavenderHeartsMultiple
   private
 
   def calculate_reduction(items, total)
-    lavender_hearts = items.select{ |item| item.name == :"Lavender Heart" }
+    lavender_hearts = count_lavender_hearts(items)
     amount = 0
     if lavender_hearts.length >= 2
       lavender_hearts.each { |lavender_heart| amount += 0.75 }
     end
     amount
+  end
+
+  def count_lavender_hearts(items)
+    items.select{ |item| item.name == :"Lavender Heart" }
   end
 
 end
